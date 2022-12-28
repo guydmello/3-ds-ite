dd924797964a48d58714f4cd1d1035b84aebd23f
unknown <guyrdmello@gmail.com>
2022-12-28 16:33:53 -0500

    3d

---
+++ my-site/src/App.js
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/yxsOsVV4MhNqo92W/scene.splinecode" />
+  );
+}
+
+
